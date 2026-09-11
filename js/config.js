b23b3ee7fcadb7376057ca6f14d0e284eba80468
Sumit Sharma <115875619+sumit4537@users.noreply.github.com>
2026-09-11 21:19:54 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
         userPoolId: 'us-east-1_EoUOsQzlV', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '68fm0preauna3pivnj9kvlcenp', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolClientId: '4r119eo2ofq028mclje1rpclnb', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'us-east-1' // e.g. us-east-2
     },
     api: {
